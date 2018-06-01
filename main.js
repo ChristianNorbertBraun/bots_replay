74e6ee45a5cbefe30158745eb3b83b4e1ecfd7e7
Christian Braun <christian.braun@kurzdigital.com>
2018-06-01 18:27:12 +0200

    Improve performance

--- main.js
+++ main.js
@@ -10,60 +10,6 @@ var texture;
 var width;
 var height;
 
-function getShader(gl, id) {
-    var shaderScript = document.getElementById(id);
-
-    if (!shaderScript) {
-        return null;
-    }
-
-    var theSource = "";
-    var currentChild = shaderScript.firstChild;
-
-    while (currentChild) {
-        if (currentChild.nodeType == 3) {
-            theSource += currentChild.textContent;
-        }
-
-        currentChild = currentChild.nextSibling;
-    }
-    var shader;
-
-    if (shaderScript.type == "x-shader/x-fragment") {
-        shader = gl.createShader(gl.FRAGMENT_SHADER);
-    } else if (shaderScript.type == "x-shader/x-vertex") {
-        shader = gl.createShader(gl.VERTEX_SHADER);
-    } else {
-        return null;
-    }
-    gl.shaderSource(shader, theSource);
-
-    gl.compileShader(shader);
-
-    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-        alert("Error while compiling the shader" + gl.getShaderInfoLog(shader));
-        return null;
-    }
-
-    return shader;
-}
-
-function initShaders() {
-    var fragmentShader = getShader(gl, "shader-fs");
-    var vertexShader = getShader(gl, "shader-vs");
-
-    var shaderProgram = gl.createProgram();
-    gl.attachShader(shaderProgram, vertexShader);
-    gl.attachShader(shaderProgram, fragmentShader);
-    gl.linkProgram(shaderProgram);
-
-    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-        alert("Error while initialising shader program");
-    }
-
-    return shaderProgram
-}
-
 // Stays here
 function configureShaders(shaderProgram) {
     gl.useProgram(shaderProgram);
@@ -73,97 +19,56 @@ function configureShaders(shaderProgram) {
     gl.uniform1i(gl.getUniformLocation(shaderProgram, "uSampler"), 0);
 }
 
-function getAndEnableGlVertexAttribArray(glContext, program, name) {
-    var attribute = glContext.getAttribLocation(program, name);
-    gl.enableVertexAttribArray(attribute);
 
-    return attribute
+var vertices = new Float32Array([
+    -1.0, 1.0,
+    -1.0, -1.0,
+    1.0, 1.0,
+    1.0, -1.0,
+]);
+
+var textureCoordinates = new Float32Array([
+    0.0, 0.0,
+    0.0, 1.0,
+    1 / 4, 0.0,
+    1 / 4, 1.0,
+
+    0.5, 0.0,
+    0.5, 1.0,
+    1.0, 0.0,
+    1.0, 1.0,
+]);
+
+var glBuffer;
+var glTextureBuffer;
+
+function initBuffers() {
+    glBuffer = gl.createBuffer();
+    glTextureBuffer = gl.createBuffer();
 }
 
-function initWebGL(canvas) {
-    gl = null;
-    try {
-        gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
-    } catch (e) {
-        console.log(e);
-    }
-
-    if (!gl) {
-        alert("Unable to initiate WebGL");
-        gl = null;
-    }
-
-    return gl;
+function activateTexture() {
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, texture);
 }
 
 function createSquare(x, y) {
-    var vertices = new Float32Array([
-        -1.0, 1.0,
-        -1.0, -1.0,
-        1.0, 1.0,
-        1.0, -1.0,
-    ]);
-
-    // var indices = [3, 2, 1, 3, 1, 0];
-
-    var textureCoordinates = new Float32Array([
-        0.0, 0.0,
-        0.0, 1.0,
-        1 / 4, 0.0,
-        1 / 4, 1.0,
-
-        0.5, 0.0,
-        0.5, 1.0,
-        1.0, 0.0,
-        1.0, 1.0,
-    ]);
-    var glTextureBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, glTextureBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, textureCoordinates, gl.STATIC_DRAW);
     // This will change from frame to frame
     gl.vertexAttribPointer(textureCoordAttribute, 2, gl.FLOAT, false, 0, 0);
 
-    var glBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, glBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
     gl.vertexAttribPointer(vertexPositionAttribute, 2, gl.FLOAT, false, 0, 0);
 
-    gl.activeTexture(gl.TEXTURE0);
-    // Here i have to use other texture
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-    var perspectiveMatrix = createPerspectiveMatrix();
     // This will change from frame to frame
     var translationMatrix = createTranslationMatrix(scaleFactor, scaleFactor, x, y);
+    gl.uniformMatrix3fv(gl.getUniformLocation(shaderProgram, "transformation"), false, translationMatrix);
+    var perspectiveMatrix = createPerspectiveMatrix(width, height);
 
     gl.uniformMatrix3fv(gl.getUniformLocation(shaderProgram, "perspective"), false, perspectiveMatrix);
-    gl.uniformMatrix3fv(gl.getUniformLocation(shaderProgram, "transformation"), false, translationMatrix);
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-}
-
-function createPerspectiveMatrix() {
-    var perspectiveMat = new Float32Array([
-        1, 0, 0,
-        0, width / height, 0,
-        0, 0, 1
-    ]);
-
-    return perspectiveMat;
-}
-
-function createTranslationMatrix(scaleFactorX, scaleFactorY, translationX, translationY) {
-    var transMat = new Float32Array([
-        1, 0, 0,
-        0, 1, 0,
-        0, 0, 1
-    ]);
-
-    transMat[0] = scaleFactorX;
-    transMat[4] = scaleFactorY;
-
-    transMat[6] = translationX * scaleFactorX;
-    transMat[7] = translationY * scaleFactorY;
-
-    return transMat;
 }
 
 var map = [
@@ -185,16 +90,6 @@ function resize() {
 
 var last = Date.now();
 function draw() {
-    // requestAnimationFrame(draw);
-
-    // var now = Date.now();
-    // if (now - last < 100) {
-    //     console.log("Not enough time gone")
-    //     return
-    // }
-    // last = now;
-   
-
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
     var startX = -(mapDimension - 1);
@@ -209,17 +104,8 @@ function draw() {
         }
         startY += 2
     }
-}
 
-function initTexture(texture, image) {
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    gl.generateMipmap(gl.TEXTURE_2D);
-    gl.bindTexture(gl.TEXTURE_2D, null);
+    requestAnimationFrame(draw);
 }
 
 function init(image) {
@@ -232,7 +118,9 @@ function init(image) {
         shaderProgram = initShaders();
         configureShaders(shaderProgram);
         texture = gl.createTexture();
-        initTexture(texture, image)
+        initTexture(gl, texture, image)
+        activateTexture();
+        initBuffers();
 
         window.onresize = resize;
         resize();
@@ -250,6 +138,6 @@ function load() {
     var image = new Image();
     image.onload = function () { init(image) };
     image.src = "atlas.png";
-} 
+}
 
 window.onload = load;
---
+++ utils.js
@@ -0,0 +1,113 @@
+function getShader(gl, id) {
+    var shaderScript = document.getElementById(id);
+
+    if (!shaderScript) {
+        return null;
+    }
+
+    var theSource = "";
+    var currentChild = shaderScript.firstChild;
+
+    while (currentChild) {
+        if (currentChild.nodeType == 3) {
+            theSource += currentChild.textContent;
+        }
+
+        currentChild = currentChild.nextSibling;
+    }
+    var shader;
+
+    if (shaderScript.type == "x-shader/x-fragment") {
+        shader = gl.createShader(gl.FRAGMENT_SHADER);
+    } else if (shaderScript.type == "x-shader/x-vertex") {
+        shader = gl.createShader(gl.VERTEX_SHADER);
+    } else {
+        return null;
+    }
+    gl.shaderSource(shader, theSource);
+
+    gl.compileShader(shader);
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert("Error while compiling the shader" + gl.getShaderInfoLog(shader));
+        return null;
+    }
+
+    return shader;
+}
+
+function initShaders() {
+    var fragmentShader = getShader(gl, "shader-fs");
+    var vertexShader = getShader(gl, "shader-vs");
+
+    var shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        alert("Error while initialising shader program");
+    }
+
+    return shaderProgram
+}
+
+function getAndEnableGlVertexAttribArray(glContext, program, name) {
+    var attribute = glContext.getAttribLocation(program, name);
+    gl.enableVertexAttribArray(attribute);
+
+    return attribute
+}
+
+function initWebGL(canvas) {
+    var gl = null;
+    try {
+        gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+    } catch (e) {
+        console.log(e);
+    }
+
+    if (!gl) {
+        alert("Unable to initiate WebGL");
+        gl = null;
+    }
+
+    return gl;
+}
+
+function initTexture(gl, texture, image) {
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.generateMipmap(gl.TEXTURE_2D);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+}
+
+function createTranslationMatrix(scaleFactorX, scaleFactorY, translationX, translationY) {
+    var transMat = new Float32Array([
+        1, 0, 0,
+        0, 1, 0,
+        0, 0, 1
+    ]);
+
+    transMat[0] = scaleFactorX;
+    transMat[4] = scaleFactorY;
+
+    transMat[6] = translationX * scaleFactorX;
+    transMat[7] = translationY * scaleFactorY;
+
+    return transMat;
+}
+
+function createPerspectiveMatrix(width, height) {
+    var perspectiveMat = new Float32Array([
+        1, 0, 0,
+        0, width / height, 0,
+        0, 0, 1
+    ]);
+
+    return perspectiveMat;
+}
